93b1f75fd595a37735117ef6888b7235197191d6
randomkishan <92361989+randomkishan@users.noreply.github.com>
2021-10-12 14:29:31 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,17 @@
+# Defining list of phrases which will help to build a story
+Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time']
+
+character = [' there lived a king.',' there was a man named Jack.',
+			' there lived a farmer.']
+
+time = [' One day', ' One full-moon night']
+
+story_plot = [' he was passing by', ' he was going for a picnic to ']
+
+place = [' the mountains', ' the garden']
+
+second_character = [' he saw a man', ' he saw a young lady']
+
+age = [' who seemed to be in late 20s', ' who seemed very old and feeble']
+
+work = [' searching something.', ' digging a well on roadside.']
