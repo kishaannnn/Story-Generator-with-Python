8061a95176c7859ea63d2967d51b712ebd36ad44
byteface <byteface@googlemail.com>
2021-10-30 12:39:10 +0100

    Change to use fstrings. Include name.
    also reduced call to random.choice to just opt for clarity

--- story.py
+++ story.py
@@ -1,8 +1,12 @@
 import random
-when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
-who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+
+opt = random.choice
+
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago', 'On 20th Jan', 'The other day', 'Many moons ago', 'About a fortnight ago']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat', 'a chinchilla with 3 eyes']
 name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
 residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
 went = ['cinema', 'university','seminar', 'school', 'laundry']
 happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
-print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+
+print( f"{opt(when)}, {opt(who)} called {opt(name)} that lived in {opt(residence)}, went to the {opt(went)} and {opt(happened)}" )
